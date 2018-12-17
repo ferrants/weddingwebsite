4cccc03804688228a8616b2996e74932d92e83f4
matt ferrante <mferrante3@gmail.com>
2018-12-16 17:12:31 -0700

    dynamic text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+const alt_quotes = [
+	"He's busy, please don't ask him to make you a website.",
+	"He is really busy, don't ask him to make you a website.",
+	"He's busy, you shouldn't ask him to make you a website.",
+	"He's busy, please don't ask him to make you a website.",
+	"He is really busy, don't ask him to make you a website.",
+	"He's busy, you shouldn't ask him to make you a website.",
+	"He's busy, please don't ask him to make you a website.",
+	"He is really busy, don't ask him to make you a website.",
+	"He's busy, you shouldn't ask him to make you a website.",
+
+	"Need a website? Ask Matt!",
+];
 
 $(function () {
 	$('a.embed').on('click', function () {
@@ -5,6 +18,14 @@ $(function () {
 		return false;
 	});
 	$('.embed-input').on("click", function () {
-    $(this).select();
+    	$(this).select();
+	});
+
+	$(document).ready(function () {
+		let quote_prefix = "This site was created by Matt Ferrante. ";
+		if (Math.random() > 0.30) {
+			let alt_quote = alt_quotes[Math.floor(Math.random() * alt_quotes.length)];
+			$('#bottom-quote').text(quote_prefix + alt_quote);
+		}
 	});
 });
--- details.md
+++ details.md
@@ -4,7 +4,7 @@ layout: default
 
 # Wedding Details
 
-Matt and Meredith will be getting married at 4pm on Saturday, August 10th, 2019 at The Cliff Lodge at Snowbird Ski Resort.
+Matt and Meredith will be getting married at 4pm on Saturday, August 10th, 2019 at The Cliff Lodge at Snowbird Ski Resort in Utah.
 Dinner and dancing will follow late into the night.
 
-Lodging will be available at the Cliff Lodge and other locations at Snowbird, in Little Cottonwood Canyon. Check back here for more details on lodging and other weekend events.
+Lodging will be available at the Cliff Lodge and other locations at Snowbird Ski Resort, in Little Cottonwood Canyon. Check back here for more details on lodging and other weekend events.
