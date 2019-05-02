6ce5109c7e2e59ea6122842e76175892cbd53cdf
matt ferrante <mferrante3@gmail.com>
2019-05-02 09:51:17 -0600

    countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,5 +29,27 @@ $(function () {
 			let alt_quote = alt_quotes[Math.floor(Math.random() * alt_quotes.length)];
 			$('#bottom-quote').text(quote_prefix + alt_quote);
 		}
+
+		wedding_date = new Date("Aug 10, 2019 4:00:00").getTime();
+		interval = setInterval(function() {
+
+		  // Get todays date and time
+		  var now = new Date().getTime();
+
+		  // Find the distance between now and the count down date
+		  var distance = wedding_date - now;
+
+		  // Time calculations for days, hours, minutes and seconds
+		  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+		  // Display the result in the element with id="demo"
+		  document.getElementById("countdown").innerHTML = days + " days until the wedding!";
+
+		  // If the count down is finished, write some text
+		  if (distance < 0) {
+			clearInterval(interval);
+			document.getElementById("countdown").innerHTML = "";
+		  }
+		}, 1000);
 	});
 });
